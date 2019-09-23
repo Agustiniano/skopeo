129d5d09a3e9fef7f24b9a0db6336bdd1943cd6c
Mark D <xphyr@users.noreply.github.com>
2019-09-23 08:46:04 -0400

    Its not *nix
    Removing commands specific to unix like OSes

--- hack/travis_windows.sh
+++ hack/travis_windows.sh
@@ -6,12 +6,12 @@ export PATH=$GOPATH/bin:$PATH
 
 _containers="${GOPATH}/src/github.com/containers"
 mkdir -vp ${_containers}
-ln -vsf $(pwd) ${_containers}/skopeo
+# ln -vsf $(pwd) ${_containers}/skopeo
 
 go version
 GO111MODULE=off go get -u github.com/cpuguy83/go-md2man golang.org/x/lint/golint
 
 cd ${_containers}/skopeo
 make validate-local test-unit-local binary-local
-sudo make install
+make install
 skopeo -v
