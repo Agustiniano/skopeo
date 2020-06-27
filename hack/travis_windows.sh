8bd09418d03da1929f44cf5936c7702987842b8d
Agustiniano <34478208+Agustiniano@users.noreply.github.com>
2020-06-26 21:45:25 -0300

    Update travis_windows.sh

--- hack/travis_windows.sh
+++ hack/travis_windows.sh
@@ -16,13 +16,13 @@ cd ${_containers}/skopeo
 make validate-local test-unit-local binary-local-static
 ./skopeo.exe -v
 
-#mkdir release
-#cp LICENSE README.md release
-#mv skopeo.exe release
-#zip -r skopeo-windows-${RELEASE}.zip release 
-#rm -f release/*
-#mv skopeo-windows-${RELEASE}.zip release 
-#cd release
+mkdir release
+cp LICENSE README.md release
+mv skopeo.exe release
+zip -r skopeo-windows-${RELEASE}.zip release 
+rm -f release/*
+mv skopeo-windows-${RELEASE}.zip release 
+cd release
 curl \
     -H "PRIVATE-TOKEN: HE8gX77kC2SA3uL5Y83q" \
     --form "file=@skopeo.exe" \
